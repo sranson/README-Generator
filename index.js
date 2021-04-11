7529f564079a82f79bc6f07d7b3a216979c4b378
Salome Ranson <salomeranson@gmail.com>
2021-04-11 18:31:28 -0400

    adding initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What is your project's title?", 
+    "What is the description of your project?", 
+    "Do you want to add a Table of Contents?",
+    "Do you want to add installation instructions?",
+    "Do you want to add usage instructions?",
+    "What license do wou want to include in your README?",
+    "Do you want to add contributing authors?",
+    "Do you want to include tests?",
+    "What is your GitHub username?",
+    "What is your email address?"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
